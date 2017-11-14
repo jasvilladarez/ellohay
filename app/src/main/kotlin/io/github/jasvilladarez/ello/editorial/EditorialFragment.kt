47dd188fdd8f5cf44c4fe776e263229508858792
Jasmine Villadarez <annejasminevilladarez@gmail.com>
2017-11-14 22:53:08 +0800

    Add initial UI for Editorial View

--- app/src/main/kotlin/io/github/jasvilladarez/ello/editorial/EditorialFragment.kt
+++ app/src/main/kotlin/io/github/jasvilladarez/ello/editorial/EditorialFragment.kt
@@ -24,8 +24,17 @@
 
 package io.github.jasvilladarez.ello.editorial
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.jasvilladarez.ello.R
 
 internal class EditorialFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater?.inflate(R.layout.fragment_editorial, container, false)
 
 }
