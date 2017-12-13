7ab7420eadca030bddf2c9dfc14db75eb7131598
Jasmine Villadarez <annejasminevilladarez@gmail.com>
2017-12-13 23:51:38 +0800

    Suppress deprecation warning for fromHtml

--- app/src/main/kotlin/io/github/jasvilladarez/ello/util/StringExtensions.kt
+++ app/src/main/kotlin/io/github/jasvilladarez/ello/util/StringExtensions.kt
@@ -31,5 +31,6 @@ import android.text.Spanned
 fun String.fromHtml(): Spanned? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
     Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
 } else {
+    @Suppress("DEPRECATION")
     Html.fromHtml(this)
 }
